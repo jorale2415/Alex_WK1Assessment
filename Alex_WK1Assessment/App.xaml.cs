430065cda5647053d02a2a60e7a3bf392e34844a
jorale2415 <60239297+jorale2415@users.noreply.github.com>
2021-04-04 21:45:38 -0500

    Add project files.

---
+++ Alex_WK1Assessment/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Alex_WK1Assessment
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Alex_WK1Assessment/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Alex_WK1Assessment
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLogin_Clicked(object sender, EventArgs e)
+        {
+            // if user id is Alex01 and password is Password1
+            // Display message "Login successful" followed by the user ID
+            string storedUserName = "Alex01";
+            string storedPassword = "Password1";
+
+            string userName = txtUserName.Text;
+            string password = txtPassword.Text;
+
+
+            if (storedUserName == userName && storedPassword == password)
+            {
+                txtOutput.Text = "Login successful, " + userName;
+            }
+            else
+            {
+                // if user id or password does not match the values above 
+                // Display message "Login failed" followed by the user ID entered
+                txtOutput.Text = "Login Failed, " + userName;
+            }    
+        }
+
+        private void ButtonCancel_Clicked(object sender, EventArgs e)
+        {
+            // Clear the User ID and Password fields and any messages that have been displayed
+            txtUserName.Text = string.Empty;
+            txtPassword.Text = string.Empty;
+            txtOutput.Text = string.Empty;
+        }
+    }
+}
